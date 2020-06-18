9536de9663d87f22f011618c1d3175827b0c78d6
Morgan Webb <mnbwebb2233@gmail.com>
2020-06-18 18:09:42 +1000

    Add turn-based clicking logic

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,12 +2,44 @@ class App {
   constructor(names, rounds) {
     this.names = names;
     this.rounds = rounds;
-    this.squares = document.querySelectorAll(".square");
-    this.squares.forEach((square) => {
-     square.style.width = 50;
-     square.style.height = 50;
-     
-    }); }
+    this.clicks = 0;
+    this.turn = names[0];
+    this.squares = Array.from(document.querySelectorAll(".square")).map(
+      (node) => {
+        const obj = {
+          square: node,
+          clicker: null
+        };
+
+        return obj;
+      }
+    );
+
+    this.squares.forEach((obj) => {
+      obj.square.addEventListener("click", this.handleClick.bind(this), {
+        once: true,
+      });
+    });
+  }
+
+  handleClick(click) {
+    this.clicks++;
+    if (this.clicks > 2) {
+      this.checkWin();
+    }
+
+    this.squares.forEach((obj) => {
+      if (click.target === obj.square) {
+        obj.clicker = this.turn;
+        console.log(obj.clicker);
+      }
+    });
+
+    this.turn = this.turn === this.names[0] ? this.names[1] : this.names[0];
+  }
+
+  checkWin() {
+  }
 }
 
-const app = new App(["Morgan", "Bot"], 3);
+const app = new App(["Morgan", "Steve"], 3);
